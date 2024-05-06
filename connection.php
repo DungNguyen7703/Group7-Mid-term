85c37b5a6deeb41a90992854e65c3a6af44c02f9
Dung Nguyen <149448159+GatGuGamingGear@users.noreply.github.com>
2024-05-06 23:53:57 +0700

    update

--- connection.php
+++ connection.php
@@ -1,13 +1,29 @@
 <?php
-    $server="localhost";
-    $user="root";
-    $pass="";
-    $dbname="exam2";
-    $ocon=new mysqli($server,$user,$pass,$dbname);
-    if($ocon->connect_error){
-        die("Lỗi kết nối".$ocon->connect_error);
+
+class Connection {
+    private $server;
+    private $user;
+    private $pass;
+    private $dbname;
+    private $ocon;
+
+    public function __construct($server, $user, $pass, $dbname) {
+        $this->server = $server;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->dbname = $dbname;
+        $this->ocon = new mysqli($this->server, $this->user, $this->pass, $this->dbname);
+        if ($this->ocon->connect_error) {
+            die("Lỗi kết nối" . $this->ocon->connect_error);
+        }
     }
-    //echo "Kết nối thành công"
-    
+
+    public function getConnection() {
+        return $this->ocon;
+    }
+}
+
+$connection = new Connection("localhost", "root", "", "exam2");
+$ocon = $connection->getConnection();
 
 ?>
