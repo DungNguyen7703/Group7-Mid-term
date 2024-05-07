202a78ce4e3daea6881dc251f43443b0ba38e161
Dung Nguyen <149448159+GatGuGamingGear@users.noreply.github.com>
2024-05-07 12:38:16 +0700

    update

--- control_login.php
+++ control_login.php
@@ -1,4 +1,6 @@
 <?php
+include "connection.php";
+
 class LoginController {
     private $ocon;
 
@@ -7,24 +9,28 @@ class LoginController {
     }
 
     public function login($user, $pass) {
-        $sql = "SELECT * FROM accounts WHERE username='$user' AND password='$pass'";
+        $user = $this->ocon->real_escape_string($user);
+        $pass = $this->ocon->real_escape_string($pass);
+        $sql = "SELECT * FROM accounts WHERE username='$user' AND password ='$pass'";
         $result = $this->ocon->query($sql);
 
         if ($result->num_rows > 0) {
             session_start();
             $row = $result->fetch_assoc();
             $_SESSION["user"] = $user;
-            $_SESSION["quyen"] = $row["quyen"];
+            $_SESSION["type"] = $row["type"];
             header("location:index.php");
         } else {
+            echo "<script>alert('Đăng nhập thất bại!')</script>";
             header("location:login.php?error=1");
         }
     }
 }
 
-$user = $_GET["user"];
-$pass = $_GET["pass"];
-include "connection.php";
+$user = $_POST["user"];
+$pass = $_POST["pass"];
 
+$ocon = new mysqli("localhost", "root", "", "exam2");
 $loginController = new LoginController($ocon);
 $loginController->login($user, $pass);
+?>
